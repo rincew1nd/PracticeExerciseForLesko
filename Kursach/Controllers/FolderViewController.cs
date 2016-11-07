084089b9732ba257ac97862d65971fffef73ecad
rincew1nd <xkorikx@gmail.com>
2016-11-07 17:49:59 +0300

    Начало курсача. Могём в открывать, удалять и копировать файлы, ходить по папкам. Будущее - удаление и копирование папок, вставка без клика по конкретному элементу, копирование без зависимости от типа объекта (файл\папка). После внедрения всех фитч провести глобальный рефакторинг (разнести и переименовать методы в соответствии с их функциональностью).

---
+++ Kursach/Controllers/FolderViewController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Kursach.Models;
+using Kursach.Properties;
+
+namespace Kursach.Controllers
+{
+    public class FolderViewController
+    {
+        private static FolderViewController _instance;
+        private TreeView _folderView;
+
+        public static FolderViewController GetInstance()
+        {
+            return _instance ?? (_instance = new FolderViewController());
+        }
+
+        private FolderViewController() { }
+        public void SetViewObject(TreeView folderView)
+        {
+            _folderView = folderView;
+            GetDrives();
+        }
+
+        public void GetDrives()
+        {
+            foreach (var driveInfo in DriveInfo.GetDrives())
+            {
+                var node = new FolderNode(driveInfo.Name, driveInfo.RootDirectory.FullName);
+                UpdateNode(node, false);
+                _folderView.Nodes.Add(node);
+            }
+        }
+
+        public void UpdateNode(TreeNode nodeDummy, bool lookDeeper)
+        {
+            var node = nodeDummy as FolderNode;
+            if (node == null) return;
+
+            node.Nodes.Clear();
+            try
+            {
+                var directories = Directory.GetDirectories(node.Path);
+                foreach (var folder in directories)
+                {
+                    var nodeNext = new FolderNode(Path.GetFileName(folder), folder);
+                    if (lookDeeper)
+                        UpdateNode(nodeNext, false);
+                    node.Nodes.Add(nodeNext);
+                }
+                if (lookDeeper)
+                    FileViewController.GetInstance().UpdateFileList(node.Path);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                if (lookDeeper)
+                    MessageBox.Show(string.Format(Resources.FolderUnauthorizedAccesException, node.Path));
+            }
+        }
+
+    }
+}
---
+++ Kursach/Models/FolderNode.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace Kursach.Models
+{
+    public class FolderNode : TreeNode
+    {
+        public string Path { get; set; }
+
+        public FolderNode(string name, string path) : base(name)
+        {
+            Path = path;
+        }
+    }
+}
